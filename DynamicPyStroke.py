a1a960365f89cebd7826d9daf4b216a7c69580e1
ShervonTho <55221650+ShervonTho@users.noreply.github.com>
2019-10-09 15:03:27 -0700

    Add files via upload

---
+++ DynamicPyStroke.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 17:58:34 2019
+
+Project Team: Dynamic PyStroke
+
+Team Menbers: Surya Kakria, Shervon Thomas, Ben Minikwu, Armin Abazari, Affad Shaikh
+
+"""
+def welcome():
+    return "Welcome, Project Team Dynamic PyStroke"
+#print('Welcome, Project Team Dynamic PyStroke')
---
+++ testwelcome.py
@@ -0,0 +1,5 @@
+import DynamicPyStroke as dp
+def test_welcome():
+    assert dp.welcome() == "Welcome, Project Team Dynamic PyStroke"
+
+    
