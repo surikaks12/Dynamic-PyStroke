a78600844a776342696af6b1bf2be513e8ea3f9d
Arminabz <55214876+Arminabz@users.noreply.github.com>
2019-11-18 15:17:34 -0800

    database conncetion in setting

--- Dynamic-PyStroke_Demo/django/myprojects/dynamicpystroke/models.py
+++ Dynamic-PyStroke_Demo/django/myprojects/dynamicpystroke/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class UserData(model.Model):
+    username= models.CharField(max_length=100)
+    userkey= models.CharField(max_length=6, primary_key=True, default=pkgen)
+    password= models.CharField(max_length=10000)
--- Dynamic-PyStroke_Demo/django/myprojects/myprojects/settings.py
+++ Dynamic-PyStroke_Demo/django/myprojects/myprojects/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myprojects.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ixvgfxto',
+        'USER': 'ixvgfxto',
+        'PASSWORD': 'Lg71aU_InBQ2vDUcmq19KuHCYIhWJAm0',
+        'HOST': 'salt.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
